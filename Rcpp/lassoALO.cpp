46b0f46d6d76addd7caee0bd2f9c86275a7bf614
Francis Hsu <Francis-Hsu@users.noreply.github.com>
2018-07-31 12:19:15 -0400

    Add files via upload
    update for generalized loss functions

--- Rcpp/lassoALO.cpp
+++ Rcpp/lassoALO.cpp
@@ -20,22 +20,73 @@ vec lassoALO(const vec &beta, const mat &X, const vec &y) {
 }
 
 //[[Rcpp::export]]
-vec elsnetALO(const vec &beta, const mat &X, const vec &y, const double &lambda, const double &alpha) {
-  // double gamma = 1 / (1 + (1 - alpha) * lambda);
+vec elnetALO(const vec &beta, const mat &X, const vec &y, const double &lambda, const double &alpha) {
   // double sy = stddev(y, 1);
   vec yhat = X * beta;
   uvec E = find(abs(beta) >= 1e-8);
   mat XE = X.cols(E);
-  mat I_EE = eye<mat>(E.n_elem, E.n_elem);
-  // mat H = gamma * XE * inv_sympd(gamma * XE.t() * XE / X.n_rows + (1 - gamma) * I_EE) * XE.t();
-  mat H = XE * inv_sympd(XE.t() * XE / X.n_rows + (1 - alpha) * lambda * I_EE) * XE.t();
+  mat hessR = (1 - alpha) * lambda * eye<mat>(E.n_elem, E.n_elem);
+  mat H = XE * inv_sympd(XE.t() * XE / X.n_rows + hessR) * XE.t();
   vec y_alo = yhat + diagvec(H) % (yhat - y) / (X.n_rows - diagvec(H));
   
   return y_alo;
 }
 
 //[[Rcpp::export]]
-vec genlassoALO(const vec &beta, const vec &u, const mat &X, const vec &y, const mat &D, const double &lambda) {
+vec lognetALO(const vec &beta, const mat &X, const vec &y, const double &lambda, const double &alpha) {
+  vec yhat = X * beta;
+  vec eXb = exp(yhat);
+  uvec E = find(abs(beta) >= 1e-8);
+  mat XE = X.cols(E);
+  mat hessR = (1 - alpha) * lambda * eye<mat>(E.n_elem, E.n_elem);
+  mat H = XE * inv_sympd(XE.t() * diagmat(eXb / square(1.0 + eXb)) * XE / X.n_rows + hessR) * XE.t();
+  vec y_alo = yhat + diagvec(H) % (eXb / (1.0 + eXb) - y) / (X.n_rows - diagvec(H) % (eXb / square(1.0 + eXb)));
+  
+  return y_alo;
+}
+
+//[[Rcpp::export]]
+// not working
+mat multnetALO(const mat &B, const mat &X, const mat &Y, const double &lambda, const double &alpha) {
+  mat XB = X * B;
+  mat eXB = exp(XB);
+  vec eXBRowSum = sum(eXB, 1);
+  mat XB_alo = XB;
+
+  vec eXb;
+  vec lossSecDev;
+  uvec E;
+  mat XE;
+  mat hessR;
+  mat H;
+  for(uword i = 0; i < B.n_cols; i++) {
+    eXb = eXB.col(i);
+    lossSecDev = eXb % (eXBRowSum - eXb) / square(eXBRowSum);
+    E = find(abs(B.col(i)) >= 1e-8);
+    XE = X.cols(E);
+    hessR = (1 - alpha) * lambda * eye<mat>(E.n_elem, E.n_elem);
+    H = XE * inv_sympd(XE.t() * diagmat(lossSecDev) * XE / X.n_rows + hessR) * XE.t();
+    XB_alo.col(i) += diagvec(H) % (eXb / eXBRowSum - Y.col(i)) / (X.n_rows - diagvec(H) % lossSecDev);
+  }
+  
+  return XB_alo;
+}
+
+//[[Rcpp::export]]
+vec fishnetALO(const vec &beta, const mat &X, const vec &y, const double &lambda, const double &alpha) {
+  vec yhat = X * beta;
+  vec eXb = exp(yhat);
+  uvec E = find(abs(beta) >= 1e-8);
+  mat XE = X.cols(E);
+  mat hessR = (1 - alpha) * lambda * eye<mat>(E.n_elem, E.n_elem);
+  mat H = XE * inv_sympd(XE.t() * diagmat(eXb) * XE / X.n_rows + hessR) * XE.t();
+  vec y_alo = yhat + diagvec(H) % (eXb - y) / (X.n_rows - diagvec(H) % eXb);
+  
+  return y_alo;
+}
+
+//[[Rcpp::export]]
+vec fusedALO(const vec &beta, const vec &u, const mat &X, const vec &y, const mat &D, const double &lambda) {
   vec yhat = X * beta;
   //uvec mE = find(abs(diff(beta)) <= 1e-4);
   uvec mE = find(abs(abs(u) - lambda) >= 1e-4);
